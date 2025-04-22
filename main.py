b99a255d054dd516f0e7787244f10ff1d9bea707
VedaSwaroop  Dasagrandhi <161063269+Vedv7@users.noreply.github.com>
2025-04-21 21:58:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ logger = logging.getLogger("formsiq.extractor")
 # --- Vertex Init ---
 load_dotenv()
 vertexai.init(project="iconic-episode-256420", location="us-central1")
-MODEL = GenerativeModel("gemini-1.5-flash-latest")
+MODEL = GenerativeModel("gemini-1.5-flash")
 
 # --- FastAPI ---
 app = FastAPI(
